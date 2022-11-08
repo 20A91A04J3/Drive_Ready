7c4502b97ccdb9d8c4e107b5d262f0c0b44f13cd
A_surya_kanthi <84856996+20A91A04J3@users.noreply.github.com>
2022-11-08 21:39:56 +0530

    Add files via upload

---
+++ sum of 1st&2nd half arrays.py
@@ -0,0 +1,14 @@
+n=int(input())
+m=list(map(int,input().strip().split()))[:n]
+c=0
+a=n//2
+s=0
+p=0
+for i in range(len(m)):
+    c+=1
+    if c<=a:
+        s=s+m[i]
+    else:
+        p=p+m[i]
+print(s)
+print(p)
